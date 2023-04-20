432cd0b88fd0051ebba86128a661f1ecbe680859
Miss-Cheese <nat.galligan@gmail.com>
2023-04-20 14:56:58 -0400

    Setup

---
+++ lib/checker.rb
@@ -0,0 +1,26 @@
+def help
+    puts "I accept the following commands:"
+    puts "- help : displays this help message"
+    puts "- exit : exits this program"
+end
+
+def exit_checker
+    puts "Thank you for using Address Checker. Goodbye."
+end
+
+def run
+    while true do
+        puts "Please enter a command:"
+        users_response = gets.strip
+
+        case users_response.downcase
+        when "help"
+            help
+        when "exit"
+            exit_checker
+            break
+        else
+            puts "Invalid entry"
+        end
+    end
+end
